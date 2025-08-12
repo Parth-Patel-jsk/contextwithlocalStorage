8862d3b4047f6263874270208f187ce8c3076c5b
parth-patel-jsk <parthp16625@gmail.com>
2025-08-12 16:26:10 +0530

    Todolist with localStorage completed using context

---
+++ src/contexts/index.js
@@ -0,0 +1,1 @@
+export {TodoContext, todoProvider, useTodo} from './todoContext' 
--- src/contexts/todoContext.js
+++ src/contexts/todoContext.js
@@ -1,18 +1,26 @@
-import { react, useContext } from "react"
+import { useContext } from "react"
+import React from "react";
 
 export const TodoContext = React.createContext({
     todos: [
         {
             id: 1,
-            todoTite: 'Learn React',
+            todo: 'Learn React',
             completed: false
         }
     ],
     addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+
+
 })
 
 
 export const useTodo = ( ) => {
     return useContext(TodoContext)
 }
+
+export const TodoProvider = TodoContext.Provider;
 
